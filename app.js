f9edab6953e1bb889a88eeeed0be3424e96077b8
Laura Perera <lauraperera.pss@gmail.com>
2021-10-21 11:19:20 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const port = 8089
+
+app.set('view engine', 'ejs')
+app.use(express.static("public"))
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get("/", function(req, res){
+    res.render("cadastro")
+})
+
+app.get("/login", function(req, res){
+    res.render("login")
+})
+
+app.post("/login_realizado", function(req, res){
+    res.send(`Nome de usuário: ${req.body.usuario} <br> Email: ${req.body.email}`)
+})
+
+app.listen(port, function(){
+    console.log("Servidor rodando na URL http://localhost:8089")
+})
